9b86d8a09ec00aaff3376b71aece36e934da28e3
manashwi07 <manashwi.24bey10042@vitbhopal.ac.in>
2026-03-31 16:49:18 +0530

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Script 3: Disk and Permission Auditor
+
+DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
+
+echo "Directory Audit Report"
+echo "--------------------------------"
+
+for DIR in "${DIRS[@]}"; do
+    if [ -d "$DIR" ]; then
+        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+        SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+        echo "$DIR => Permissions: $PERMS | Size: $SIZE"
+    else
+        echo "$DIR does not exist"
+    fi
+done
+
+echo "--------------------------------"
+
+# Check Git config file
+CONFIG_FILE="$HOME/.gitconfig"
+
+if [ -f "$CONFIG_FILE" ]; then
+    PERMS=$(ls -l $CONFIG_FILE | awk '{print $1, $3, $4}')
+    echo "Git Config File => Permissions: $PERMS"
+else
+    echo "Git config file not found"
+fi
